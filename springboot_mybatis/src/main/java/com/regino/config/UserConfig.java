fc951c9122a2555d000683ef21245766acfdfdac
zhaozirui <806262720@qq.com>
2020-07-17 05:54:58 +0800

    springboot自动配置，Conditional派生注解，测试通过

--- springboot_mybatis/src/main/java/com/regino/config/UserConfig.java
+++ springboot_mybatis/src/main/java/com/regino/config/UserConfig.java
@@ -1,14 +1,32 @@
 package com.regino.config;
 
 import com.regino.pojo.User;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UserConfig {
+    /*
+        原生注解:
+            @Conditional
+                注解中需要添加一个实现类condition接口的类字节码
+        派生注解/衍生注解
+            @ConditionalOnBean(Apple.class)：
+                从当前环境中查询是否存在Apple Bean,如果存在则返回true,否则false
+            @ConditionalOnMissingBean(Apple.class):
+                从当前环境中查询是否存在Apple Bean,如果存在则返回false,否则true
+            @ConditionalOnProperty(name = "author", havingValue = "regino")
+                从配置文件中查找key是author ,val是regino,如果满足则返回true,如果不满足则返回false
+
+     */
     @Bean
-    @Conditional(MyCondition.class)
+    //@Conditional(MyCondition.class)
+    //@ConditionalOnBean(MyCondition.class)
+    //@ConditionalOnMissingBean(MyCondition.class)
+    @ConditionalOnProperty(name = "author", havingValue = "regino")
     public User user() {
         User user = new User();
         user.setId(23);
