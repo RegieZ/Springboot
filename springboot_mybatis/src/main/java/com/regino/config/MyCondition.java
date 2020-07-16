3e14fd0e42a82adc1239d7624042d22c17aa8451
zhaozirui <806262720@qq.com>
2020-07-17 05:38:57 +0800

    springboot自动配置，Conditional原生注解增加redis判断，测试通过

--- springboot_mybatis/src/main/java/com/regino/config/MyCondition.java
+++ springboot_mybatis/src/main/java/com/regino/config/MyCondition.java
@@ -6,8 +6,24 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class MyCondition implements Condition {
 
+        /*
+        需求：
+            根据环境中是否有data-redis注入user
+            如果导入了redis坐标则注入user  bean
+            如果没有导入了redis坐标则不注入user  bean
+     */
+
     @Override
-    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
-        return true;
+    public boolean matches(ConditionContext conditionContext,
+                           AnnotatedTypeMetadata annotatedTypeMetadata) {
+        boolean flag = true;
+        try {
+            //通过反射创建redisTemplate，如果没有依赖redis坐标必然报错
+            Class<?> aClass = Class.forName("org.springframework.data.redis.core.RedisTemplate");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            flag = false;
+        }
+        return flag;
     }
 }
