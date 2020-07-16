95a870a5b530bd2759f8d56dc6b2e4710e79db32
zhaozirui <806262720@qq.com>
2020-07-17 04:54:46 +0800

    springboot整合RestTemplate，测试通过

--- springboot_mybatis/src/main/java/com/regino/SpringbootMybatisApplication.java
+++ springboot_mybatis/src/main/java/com/regino/SpringbootMybatisApplication.java
@@ -2,6 +2,8 @@ package com.regino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringbootMybatisApplication {
@@ -10,4 +12,9 @@ public class SpringbootMybatisApplication {
         SpringApplication.run(SpringbootMybatisApplication.class, args);
     }
 
+    // 在启动类中将restTemplate注入到spring容器
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- springboot_mybatis/src/test/java/com/regino/SpringbootMybatisApplicationTests.java
+++ springboot_mybatis/src/test/java/com/regino/SpringbootMybatisApplicationTests.java
@@ -2,15 +2,38 @@ package com.regino;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootMybatisApplicationTests {
 
+    // 由于没有采用自动配置，所以需要手动创建注入容器
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Test
     public void contextLoads() {
+		/*
+		第一个参数： 请求地址
+		第二个参数： 返回值类型
+		 */
+        String forObject = restTemplate.getForObject("http://www.baidu.com", String.class);
+        System.out.println(forObject);
+    }
+
+    @Test
+    public void testFindAllStr() {
+        String forObject = restTemplate.getForObject("http://localhost:8082/findAllStr", String.class);
+        System.out.println(forObject);
+        System.out.println("==============================");
+        List list = restTemplate.getForObject("http://localhost:8082/findAll", List.class);
+        System.out.println(list);
     }
 
 }
