4391ad6742e721a01773401493ba87a92b622e48
zhaozirui <806262720@qq.com>
2020-07-17 05:31:18 +0800

    springboot自动配置，Conditional原生注解使用，测试通过

---
+++ springboot_mybatis/src/main/java/com/regino/config/MyCondition.java
@@ -0,0 +1,13 @@
+package com.regino.config;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class MyCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        return true;
+    }
+}
--- springboot_mybatis/src/main/java/com/regino/config/UserConfig.java
+++ springboot_mybatis/src/main/java/com/regino/config/UserConfig.java
@@ -2,11 +2,13 @@ package com.regino.config;
 
 import com.regino.pojo.User;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UserConfig {
     @Bean
+    @Conditional(MyCondition.class)
     public User user() {
         User user = new User();
         user.setId(23);
