9c0adabe17c92315f8eb39d38f2e8a4299a3ebc1
zhaozirui <806262720@qq.com>
2020-07-17 03:32:54 +0800

    springboot整合redis，测试通过

--- springboot_mybatis/src/main/java/com/regino/controller/UserController.java
+++ springboot_mybatis/src/main/java/com/regino/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.regino.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.regino.pojo.User;
 import com.regino.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +17,40 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @GetMapping("findAll")
     public List<User> findAll(){
         return userService.findAll();
+    }
+
+    @GetMapping("/getRedis")
+    public String getRedis(){
+        //设值
+        redisTemplate.boundValueOps("springboot").set("regino");
+        //获取
+        String str = (String) redisTemplate.boundValueOps("springboot").get();
+        return str;
+    }
+
+    // 使用redis做缓存
+    @GetMapping("findAllStr")
+    public String findAllStr(){
+
+        String userListStr = "";
+        //1.从redis中获取userList
+        userListStr = (String) redisTemplate.boundValueOps("springboot2").get();
+        //2.获取到直接返回
+        if(StringUtils.isEmpty(userListStr)){
+            System.out.println("缓存中没有，从数据库中查询");
+            //3.获取不到从数据库中查询
+            List<User> userList = userService.findAll();
+            //4.将结果序列化
+            userListStr = JSON.toJSONString(userList);
+            //5.将userList json 存放到redis中
+            redisTemplate.boundValueOps("springboot2").set(userListStr);
+        }
+        return userListStr;
     }
 }
