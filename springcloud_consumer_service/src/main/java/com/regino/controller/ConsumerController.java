c26d727bbc49de0b6bef2179782040046f4433ab
zhaozirui <806262720@qq.com>
2020-07-20 16:29:07 +0800

    springboot，spring cloud，Eureka，改造消费者通过注册中心调用，编码及测试完成

--- springcloud_consumer_service/src/main/java/com/regino/controller/ConsumerController.java
+++ springcloud_consumer_service/src/main/java/com/regino/controller/ConsumerController.java
@@ -3,13 +3,14 @@ package com.regino.controller;
 import com.regino.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,14 +30,16 @@ public class ConsumerController {
      * @NewDescription: 根据id查询用户
      **/
     @GetMapping("findUserById/{id}")
-    public User findUserById(@PathVariable("id")Integer id){
+    public User findUserById(@PathVariable("id") Integer id) {
 
         /*
             1.url硬编码(写死了) --> springcloud euraka（注册中心）
             2.负载均衡问题 --> springcloud ribbon（负载均衡器）
             3.无法感知调用服务的状态 --> springcloud hystrix（熔断器）
          */
-        String url = "http://localhost:9091/user/findUserById/" + id;
+        List<ServiceInstance> instances = discoveryClient.getInstances("SPRINGCLOUD_USER_SERVICE");
+        URI uri = instances.get(0).getUri();
+        String url = uri + "/user/findUserById/" + id;
         User user = restTemplate.getForObject(url, User.class);
         return user;
     }
@@ -45,7 +48,7 @@ public class ConsumerController {
         返回服务列表
      */
     @GetMapping("/getInstance")
-    public List<ServiceInstance> getInstance(){
+    public List<ServiceInstance> getInstance() {
         List<ServiceInstance> instances = discoveryClient.getInstances("SPRINGCLOUD_USER_SERVICE");
         return instances;
     }
