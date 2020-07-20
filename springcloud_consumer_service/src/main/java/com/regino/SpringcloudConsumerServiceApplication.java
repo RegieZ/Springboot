b736ba7c9d162565fc6f50aa5e657097254e9cb6
zhaozirui <806262720@qq.com>
2020-07-20 15:34:57 +0800

    springboot，spring cloud，Eureka，改造服务到注册中心，编码及测试完成

--- springcloud_consumer_service/src/main/java/com/regino/SpringcloudConsumerServiceApplication.java
+++ springcloud_consumer_service/src/main/java/com/regino/SpringcloudConsumerServiceApplication.java
@@ -2,10 +2,17 @@ package com.regino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+/*
+    共同点：都可以声明是一个客户端
+    @EnableDiscoveryClient：eureka/zk
+    @EnableEurekaClient: 只针对eureka客户端生效
+ */
+@EnableEurekaClient //声明是一个客户端
 public class SpringcloudConsumerServiceApplication {
 
     public static void main(String[] args) {
--- springcloud_user_service/src/main/java/com/regino/SpringcloudUserServiceApplication.java
+++ springcloud_user_service/src/main/java/com/regino/SpringcloudUserServiceApplication.java
@@ -2,8 +2,15 @@ package com.regino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+/*
+    共同点：都可以声明是一个客户端
+    @EnableDiscoveryClient：eureka/zk
+    @EnableEurekaClient: 只针对eureka客户端生效
+ */
+@EnableEurekaClient //声明是一个客户端
 public class SpringcloudUserServiceApplication {
 
     public static void main(String[] args) {
