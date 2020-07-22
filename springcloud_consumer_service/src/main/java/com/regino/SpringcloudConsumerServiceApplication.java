c99aaf981af4e3705dda5644cc8ba6751de8fa2a
zhaozirui <806262720@qq.com>
2020-07-22 12:25:55 +0800

    springboot，spring cloud，Hystrix 熔断器监控平台

--- springcloud_consumer_service/src/main/java/com/regino/SpringcloudConsumerServiceApplication.java
+++ springcloud_consumer_service/src/main/java/com/regino/SpringcloudConsumerServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient //声明是一个客户端
 @EnableCircuitBreaker //开启熔断器*/
 @SpringCloudApplication
+@EnableHystrixDashboard //开启仪表盘监控，- http://localhost:8081/hystrix -填写 http://localhost:8081/actuator/hystrix.stream
 public class SpringcloudConsumerServiceApplication {
 
     public static void main(String[] args) {
