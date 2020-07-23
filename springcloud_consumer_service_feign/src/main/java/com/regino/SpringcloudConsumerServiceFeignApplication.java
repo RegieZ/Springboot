4fc86711dcfb965d970cce551ac94f22a1dafd9d
zhaozirui <806262720@qq.com>
2020-07-23 16:56:57 +0800

    springboot，spring cloud，Feign，修改原有消费者服务

---
+++ springcloud_consumer_service_feign/src/main/java/com/regino/SpringcloudConsumerServiceFeignApplication.java
@@ -0,0 +1,25 @@
+package com.regino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringcloudConsumerServiceFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConsumerServiceFeignApplication.class, args);
+    }
+
+    //注册RestTemplate
+    @Bean
+    @LoadBalanced //开启负载均衡 ---> 启动Ribbon
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
---
+++ springcloud_consumer_service_feign/src/main/java/com/regino/controller/ConsumerController.java
@@ -0,0 +1,46 @@
+package com.regino.controller;
+
+import com.regino.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("consumer")
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("findUserById/{id}")
+    //@HystrixCommand(fallbackMethod = "fallbackMethod") //声明默认失败方法
+    public User findUserById(@PathVariable("id") Integer id) {
+
+        /*
+            1.url硬编码(写死了) --> springcloud euraka（注册中心）
+            2.负载均衡问题 --> springcloud ribbon（负载均衡器）
+            3.无法感知调用服务的状态 --> springcloud hystrix（熔断器）
+         */
+
+        /*String url = "http://localhost:9091/user/findUserById/" + id;*/
+
+        /*List<ServiceInstance> instances = discoveryClient.getInstances("SPRINGCLOUD_USER_SERVICE");
+        URI uri = instances.get(0).getUri();
+        String url = uri + "/user/findUserById/" + id;*/
+
+        String url = "http://springcloud-user-service/user/findUserById/" + id;
+
+        User user = restTemplate.getForObject(url, User.class);
+        return user;
+    }
+}
