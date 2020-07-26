75d0883a321a809b5ad99895bcd0c80cc2701cb0
zhaozirui <806262720@qq.com>
2020-07-26 10:53:47 +0800

    springboot，spring cloud，Feign 日志

---
+++ springcloud_consumer_service_feign/src/main/java/com/config/FeignLogConfig.java
@@ -0,0 +1,17 @@
+package com.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //声明是一个配置类
+public class FeignLogConfig {
+
+    /*
+        声明feign日志级别
+     */
+    @Bean
+    public Logger.Level configLog(){
+        return Logger.Level.FULL;//None, Basic, Headers, Full
+    }
+}
--- springcloud_consumer_service_feign/src/main/java/com/regino/service/ConsumerService.java
+++ springcloud_consumer_service_feign/src/main/java/com/regino/service/ConsumerService.java
@@ -1,12 +1,15 @@
 package com.regino.service;
 
+import com.config.FeignLogConfig;
 import com.regino.pojo.User;
 import com.regino.service.impl.ConsumerServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value="springcloud-user-service",fallback = ConsumerServiceImpl.class)
+@FeignClient(value="springcloud-user-service"
+        ,fallback = ConsumerServiceImpl.class
+        ,configuration = FeignLogConfig.class)//configuration可省略
 public interface ConsumerService {
 
     /*
