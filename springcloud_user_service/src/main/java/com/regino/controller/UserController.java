b5bab36947b155d96d2171390259b73cdd0c769c
zhaozirui <806262720@qq.com>
2020-07-29 12:09:07 +0800

    springboot，spring cloud，config，手动刷新配置文件（使用postman发送post请求刷新配置 http://localhost:9091/actuator/refresh）

--- springcloud_user_service/src/main/java/com/regino/controller/UserController.java
+++ springcloud_user_service/src/main/java/com/regino/controller/UserController.java
@@ -4,6 +4,7 @@ import com.regino.pojo.User;
 import com.regino.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RefreshScope //开启刷新
 public class UserController {
 
     @Autowired
