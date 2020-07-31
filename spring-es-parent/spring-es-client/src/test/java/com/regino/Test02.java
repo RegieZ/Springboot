f0895fb16d085b6c52910560394c00e1928a2d3b
zhaozirui <806262720@qq.com>
2020-07-31 23:30:33 +0800

    spring boot, elasticsearch, 查询（matchAll）

--- spring-es-parent/spring-es-client/src/test/java/com/regino/Test02.java
+++ spring-es-parent/spring-es-client/src/test/java/com/regino/Test02.java
@@ -28,14 +28,14 @@ public class Test02 {
                         new HttpHost("localhost", 9200, "http")));
     }
 
-    //匹配查询
+    //匹配查询（match）
     @Test
     public void search() {
-        //1.创建搜索请求对象 SearchRequest
+        //1.创建搜索请求对象 SearchRequest（封装的搜索请求对象）
         SearchRequest searchRequest = new SearchRequest("reggie");
-        //2.创建 SearchSourceBuilder
+        //2.创建 SearchSourceBuilder（指定排序、高亮、分页）
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        //3.创建 QueryBuilder 对象，通过工厂方法创建，不需要new
+        //3.创建 QueryBuilder 对象，通过工厂方法创建，不需要new（指定查询类型 match/match all/term/terms）
         QueryBuilder queryBuilder = QueryBuilders.matchQuery("title", "苹果");
         searchSourceBuilder.query(queryBuilder);
         searchRequest.source(searchSourceBuilder);
@@ -46,6 +46,33 @@ public class Test02 {
             SearchHit[] hits = searchResponse.getHits().getHits();
             for (SearchHit hit : hits) {
                 String sourceAsString = hit.getSourceAsString();
+                System.out.println("结果：" + sourceAsString);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //匹配查询（matchAll）（巩固）
+    @Test
+    public void matchAll() {
+        //1.创建searchRequest对象，指定索引库名称
+        SearchRequest searchRequest = new SearchRequest("reggie");
+        //2.创建searchSourceBuilder对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //3.创建queryBuilder对象，指定match all查询
+        QueryBuilder queryBuilder = QueryBuilders.matchAllQuery();
+        //4.将queryBuilder对象封装到searchSourceBuilder对象
+        searchSourceBuilder.query(queryBuilder);
+        //5.将searchSourceBuilder对象封装到searchRequest对象
+        searchRequest.source(searchSourceBuilder);
+        try {
+            //6.使用client通信
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            //7.解析结果
+            SearchHit[] hits = searchResponse.getHits().getHits();
+            for (SearchHit searchHit : hits) {
+                String sourceAsString = searchHit.getSourceAsString();
                 System.out.println("结果：" + sourceAsString);
             }
         } catch (IOException e) {
