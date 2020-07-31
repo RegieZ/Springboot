fc3976ff86fe8c434400067cc3c1ee89d07d924e
zhaozirui <806262720@qq.com>
2020-07-31 23:37:16 +0800

    spring boot, elasticsearch, 查询，提取公共方法

--- spring-es-parent/spring-es-client/src/test/java/com/regino/Test02.java
+++ spring-es-parent/spring-es-client/src/test/java/com/regino/Test02.java
@@ -31,12 +31,25 @@ public class Test02 {
     //匹配查询（match）
     @Test
     public void search() {
+        //3.创建 QueryBuilder 对象，通过工厂方法创建，不需要new（指定查询类型 match/match all/term/terms）
+        QueryBuilder queryBuilder = QueryBuilders.matchQuery("title", "苹果");
+        commonSearch(queryBuilder);
+    }
+
+    //匹配查询（matchAll）（巩固）
+    @Test
+    public void matchAll() {
+        //3.创建queryBuilder对象，指定match all查询
+        QueryBuilder queryBuilder = QueryBuilders.matchAllQuery();
+        commonSearch(queryBuilder);
+    }
+
+    //公共查询方法
+    private void commonSearch(QueryBuilder queryBuilder) {
         //1.创建搜索请求对象 SearchRequest（封装的搜索请求对象）
         SearchRequest searchRequest = new SearchRequest("reggie");
         //2.创建 SearchSourceBuilder（指定排序、高亮、分页）
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        //3.创建 QueryBuilder 对象，通过工厂方法创建，不需要new（指定查询类型 match/match all/term/terms）
-        QueryBuilder queryBuilder = QueryBuilders.matchQuery("title", "苹果");
         searchSourceBuilder.query(queryBuilder);
         searchRequest.source(searchSourceBuilder);
         try {
@@ -46,33 +59,6 @@ public class Test02 {
             SearchHit[] hits = searchResponse.getHits().getHits();
             for (SearchHit hit : hits) {
                 String sourceAsString = hit.getSourceAsString();
-                System.out.println("结果：" + sourceAsString);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //匹配查询（matchAll）（巩固）
-    @Test
-    public void matchAll() {
-        //1.创建searchRequest对象，指定索引库名称
-        SearchRequest searchRequest = new SearchRequest("reggie");
-        //2.创建searchSourceBuilder对象
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        //3.创建queryBuilder对象，指定match all查询
-        QueryBuilder queryBuilder = QueryBuilders.matchAllQuery();
-        //4.将queryBuilder对象封装到searchSourceBuilder对象
-        searchSourceBuilder.query(queryBuilder);
-        //5.将searchSourceBuilder对象封装到searchRequest对象
-        searchRequest.source(searchSourceBuilder);
-        try {
-            //6.使用client通信
-            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-            //7.解析结果
-            SearchHit[] hits = searchResponse.getHits().getHits();
-            for (SearchHit searchHit : hits) {
-                String sourceAsString = searchHit.getSourceAsString();
                 System.out.println("结果：" + sourceAsString);
             }
         } catch (IOException e) {
