59401039f2d0f33af7d3a02d725d5f16e3c309fb
zhaozirui <806262720@qq.com>
2020-07-31 15:11:06 +0800

    springboot，elasticsearch，项目搭建

---
+++ springesparent/src/main/java/com/regino/SpringEsClientApplication.java
@@ -0,0 +1,13 @@
+package com.regino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEsClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEsClientApplication.class, args);
+    }
+
+}
