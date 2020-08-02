ccc3466d13b063620084c58326bc76ae81181698
zhaozirui <806262720@qq.com>
2020-08-03 00:50:27 +0800

    spring boot, Rabbitmq，优化，项目搭建

---
+++ rabbitmq-parent/spring-mq-confirm/src/main/java/com/regino/SpringMqConfirmApplication.java
@@ -0,0 +1,13 @@
+package com.regino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMqConfirmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMqConfirmApplication.class, args);
+    }
+
+}
