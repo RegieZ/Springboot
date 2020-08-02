846267d61c554982647db1965b2b2ae1bf7ea844
zhaozirui <806262720@qq.com>
2020-08-02 16:49:03 +0800

    spring boot, Rabbitmq，简单模式"hello_world"发送对象信息，接收+自由发送（队列阅后即焚，无法查看）

--- rabbitmq-parent/spring-boot-rabbitmq/src/main/java/com/regino/consumer/ConsumerListener.java
+++ rabbitmq-parent/spring-boot-rabbitmq/src/main/java/com/regino/consumer/ConsumerListener.java
@@ -1,5 +1,6 @@
 package com.regino.consumer;
 
+import com.regino.pojo.User;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,13 @@ import org.springframework.stereotype.Component;
 @RabbitListener(queues = "simple_queue") //设置监听的队列（ProductController的routingKey）
 public class ConsumerListener {
 
-    @RabbitHandler //用于转换
+    @RabbitHandler //用于转换，String就转成String，对象就转成对象
     public void receiveMsg(String msg) {//String对应的是ProductController发的是String类型字符串（object）
+        System.out.println("Consumer has received the message: " + msg);
+    }
+
+    @RabbitHandler //用于转换
+    public void receiveUser(User msg) {//String对应的是ProductController发的是String类型字符串（object）
         System.out.println("Consumer has received the message: " + msg);
     }
 }
--- rabbitmq-parent/spring-boot-rabbitmq/src/main/java/com/regino/product/ProductController.java
+++ rabbitmq-parent/spring-boot-rabbitmq/src/main/java/com/regino/product/ProductController.java
@@ -1,5 +1,6 @@
 package com.regino.product;
 
+import com.regino.pojo.User;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,18 @@ public class ProductController {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    //发送字符串类型消息
     @GetMapping("/sendMsg/{msg}")//接收打出来的url
-    public void sendMsg(@PathVariable String msg){
-        rabbitTemplate.convertAndSend("simple_queue",msg);//简单模式下，routingKey就是队列名称
+    public void sendMsg(@PathVariable String msg) {
+        rabbitTemplate.convertAndSend("simple_queue", msg);//简单模式下，routingKey就是队列名称
+    }
+
+    //发送对象类型消息
+    @GetMapping("/sendUserMsg/{msg}")//接收打出来的url
+    public void sendMsg() {
+        User user = new User();
+        user.setName("Reggie");
+        user.setAge(18);
+        rabbitTemplate.convertAndSend("simple_queue", user);//简单模式下，routingKey就是队列名称
     }
 }
